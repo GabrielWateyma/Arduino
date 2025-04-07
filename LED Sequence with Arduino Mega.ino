664ca54d6c0eef7a8a54604d83629fb169bd9f99
GabrielWateyma <wateymagabriel@gmail.com>
2025-04-06 21:16:16 -0400

    Update LED Sequence with Arduino Mega.ino

--- LED Sequence with Arduino Mega.ino
+++ LED Sequence with Arduino Mega.ino
@@ -1,24 +1,24 @@
-// Pines de los LEDs
+// LED Pins / Pines de los LEDs
 int leds[] = {2, 3, 4, 5, 6, 7, 8, 9};
 int numLeds = 8;
 
 void setup() {
-  // Configura los pines de los LEDs como salidas
+  // Configure LED pins as outputs / Configura los pines de los LEDs como salidas
   for (int i = 0; i < numLeds; i++) {
     pinMode(leds[i], OUTPUT);
   }
 }
 
 void loop() {
-  // Secuencia de encendido y apagado de LEDs
+  // LED on/off sequence / Secuencia de encendido y apagado de LEDs
   for (int i = 0; i < numLeds; i++) {
     digitalWrite(leds[i], HIGH);
-    delay(200); // Espera 200 milisegundos
+    delay(200); // Wait 200 milliseconds / Espera 200 milisegundos
     digitalWrite(leds[i], LOW);
-    delay(100); // Espera 100 milisegundos
+    delay(100); // Wait 200 milliseconds / Espera 100 milisegundos
   }
 
-  // Secuencia inversa
+  // Reverse sequence / Secuencia inversa
   for (int i = numLeds - 1; i >= 0; i--) {
     digitalWrite(leds[i], HIGH);
     delay(200);
